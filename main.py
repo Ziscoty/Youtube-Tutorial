28b05344aae44a8ced87ca206f65cfe51bfd8724
Ziscoty <112914468+Ziscoty@users.noreply.github.com>
2022-11-30 00:53:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import bot
 
 if __name__ == '__main__':
-    bot.aclient.run(token='MTA0NjgzMjI0MTE3MjU1Nzg3NA.GxTMbP.ld3O5P_opImv-lAv9OuYzeuhiXn5LTOgxnQtC4')
+    bot.aclient.run(token='seu token')
